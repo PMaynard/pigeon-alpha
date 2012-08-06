9b9015d68deca35ca90a64825f0c0b9ab124c954
Pete Maynard <pgm987@gmail.com>
2012-08-06 20:47:21 +0100

    Added page support.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <!-- Start the Loop. -->
  <?php if ( have_posts() ) : ?>
 
- <?php if ( !(is_paged() or is_search()) ) : ?>
+ <?php if ( !(is_paged() or is_search() or is_page()) ) : ?>
 
   <?php $my_query = new WP_Query('posts_per_page=1');
   while ($my_query->have_posts()) : $my_query->the_post();
@@ -22,17 +22,20 @@
 
  <?php endif; ?>
 
-<?php if(is_search()) echo "<h1>Searching for: " . get_search_query() . "</h1>"; ?>
+ <?php if(is_search()) echo "<h1>Searching for: " . get_search_query() . "</h1>"; ?>
+ <?php if(is_page()) echo "<h1>" . wp_title("",false) . "</h1>"; ?>
 <div class="excerpt">
 
  <?php while ( have_posts() ) : the_post(); if( $post->ID == $do_not_duplicate ) continue; ?>
 
  <div class="small-post">
+
+  <?php if (!is_page()) : /* Make sure that this does not get displayed for pages */?>
    <div class="post-title"><h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2></div>
    <small><?php the_time('jS F y') ?> | <?php the_category(', '); ?></small>
-
+  <?php endif; ?>
    <div class="entry">
-     <?php the_excerpt(); ?>
+     <?php (is_page() ? the_content() : the_excerpt() );?>
    </div>
 
  </div>  <!-- End small-post -->
