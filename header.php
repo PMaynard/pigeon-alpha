c089488173209025f3aab24d502a102c1d9228c6
root <pgm987@gmail.com>
2013-03-19 10:37:50 +0000

    Added Internet Defence Leauge

--- header.php
+++ header.php
@@ -58,6 +58,19 @@
 
 <script src="http://code.jquery.com/jquery-latest.js"></script> 
 
+<!-- Internet Defense League -->
+<script type="text/javascript">
+    window._idl = {};
+    _idl.variant = "banner";
+    (function() {
+	var idl = document.createElement('script');
+	idl.type = 'text/javascript';
+	idl.async = true;
+	idl.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'members.internetdefenseleague.org/include/?url=' + (_idl.url || '') + '&campaign=' + (_idl.campaign || '') + '&variant=' + (_idl.variant || 'banner');
+	document.getElementsByTagName('body')[0].appendChild(idl);
+    })();
+</script>
+
 </head>
 
 <body>
